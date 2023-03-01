8f22cc77c887e194314d2bb659b959843312d631
unknown <mahmoud0597659286face>
2023-03-01 18:48:35 +0200

    SComment

--- app/src/main/java/mahmoud/naji/lab_computing_activity1/MainActivity.kt
+++ app/src/main/java/mahmoud/naji/lab_computing_activity1/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //save to firebase
         btnSave.setOnClickListener {
             var name = edtName.text.toString()
             var id = edtId.text.toString()
